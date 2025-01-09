fa27794b5decc1d2eba125e6c5b8fdefcfa7c7e5
djumanov <djumanovdev@gmail.com>
2025-01-09 14:40:01 +0500

    mkdir, touch

---
+++ vfsshell.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct Node {
+    char name[128];
+    int is_file;
+    int created_at; 
+    struct Node *parent;
+    struct Node *child;
+    struct Node *next;
+} Node;
+
+Node *root;
+Node *current;
+
+// utils
+void init();
+void prompt();
+Node *create_node(char name[], int is_file);
+void insert_node(Node *node);
+
+// commands
+void mkdir(char name[]);
+void touch(char name[]);
+
+int main() {
+    
+    init();
+
+    char command[64]; 
+    char arg[128]; 
+
+    while (1) {
+        prompt();
+
+        scanf("%s", command);
+
+        if (strcmp(command, "mkdir") == 0) {
+            scanf("%s", arg);
+            mkdir(arg);
+        } else if (strcmp(command, "touch") == 0) {
+            scanf("%s", arg);
+            touch(arg);
+        }
+    }
+
+    return 0;
+}
+
+void init() {
+    Node *node = malloc(sizeof(Node));
+
+    strcpy(node->name, "root");
+    node->is_file = 0;
+    node->created_at = time(NULL);
+    node->parent = NULL;
+    node->child = NULL;
+    node->next = NULL;
+
+    root = node;
+    current = root;
+}
+
+void prompt() {
+    printf("➜ %s ", current->name);
+}
+
+Node *create_node(char name[], int is_file) {
+    Node *node = malloc(sizeof(Node));
+
+    strcpy(node->name, name);
+    node->is_file = is_file;
+    node->created_at = time(NULL);
+    node->parent = NULL;
+    node->child = NULL;
+    node->next = NULL;
+
+    return node;
+}
+
+void insert_node(Node *node) {
+    if (current->child == NULL) {
+        current->child = node;
+    }
+    else {
+        Node *temp = current->child;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = node;
+    }
+}
+
+void mkdir(char name[]) {
+    Node *node = create_node(name, 0);
+
+    insert_node(node);
+}
+
+void touch(char name[]) {
+    Node *node = create_node(name, 1);
+
+    insert_node(node);
+}
